9c0feab3a9da5682ca076d188f7c4fcb2d14023e
devops <ilyaefish@gmail.com>
2019-01-19 13:11:09 +0200

    some fix

--- face_script.py
+++ face_script.py
@@ -11,11 +11,11 @@ ALL_PHOTOS = PATH + "/all_photos/"
 MY_PHOTOS = PATH + "/my_photos/"
 MY_FOUND_PIC = PATH + "/my_found_pic/"
 TEMP_FOLDER = PATH + "/tmp/"
+
 """This is the tolerance (or threshold) of the algorithm. 
 Higher tolerance tells the algorithm to be less strict, while lower means the opposite
 """
-TOLARANCE = 0.7
-YOUR_FACE_PIC = MY_PHOTOS + "ilya.jpg"
+TOLARANCE = 0.6
 
 # copy all jpg files to tmp folder
 for root, dirs, files in os.walk(ALL_PHOTOS):
@@ -29,24 +29,23 @@ cmd = "./rename_tmp_files.sh"
 
 results = subprocess.run(
     cmd, shell=True, universal_newlines=True, check=True)
-print(results.stdout)
 
 my_face_encoding = []
-
 
 my_faces = glob.glob(MY_PHOTOS + "*.jpg")
 # Create an encoding of my facial features that can be compared to other faces
 for i in my_faces:
-    picture_of_me = face_recognition.load_image_file(YOUR_FACE_PIC)
+    picture_of_me = face_recognition.load_image_file(i)
     my_face_encoding.append(face_recognition.face_encodings(picture_of_me)[0])
 
 file_count = len(os.listdir(TEMP_FOLDER))
+print("You have " + str(file_count) + " pictures")
+match_count = 0
 
 # # Iterate through all your pictures
 for i in range(1, file_count):
-    # Construct the picture name and print it
+    # Construct the pictures name
     file_name = TEMP_FOLDER + str(i).zfill(5) + ".jpg"
-    print(file_name)
 
     # Load this picture
     new_picture = face_recognition.load_image_file(file_name)
@@ -55,11 +54,14 @@ for i in range(1, file_count):
     for face_encoding in face_recognition.face_encodings(new_picture):
 
         # Run the algorithm of face comaprison for the detected face, with 0.5 tolerance
-        for y in my_face_encoding:
+        for j, y in enumerate(my_face_encoding):
             results = face_recognition.compare_faces([y], face_encoding, TOLARANCE)
-
-            # Save the image to a seperate folder if there is a match
+            print("comparing " + os.path.basename(file_name) + "    with:" + os.path.basename(my_faces[j]))
+            # Save the image to a dedicated folder if there is a match
             if results[0]:
                 print("Found one")
+                match_count += 1
                 copyfile(file_name, MY_FOUND_PIC + str(i) + ".jpg")
                 break
+
+print("We found " + str(match_count) + " of your pictures, find them in: " + MY_FOUND_PIC)
